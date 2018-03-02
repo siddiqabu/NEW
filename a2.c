6c45f4783dd94002243edf071a310b9f17d23513
siddiqabu <35294835+siddiqabu@users.noreply.github.com>
2018-03-02 15:58:46 +0530

    Create a2.c

---
+++ a2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n1, n2, min;
+  
+    scanf("%d %d", &n1, &n2);
+
+    
+    min = (n1>n2) ? n1 : n2;
+
+   
+    while(1)
+    {
+        if( min%n1==0 && min%n2==0 )
+        {
+            printf(" %d",minMultiple);
+            break;
+        }
+        ++min;
+    }
+    return 0;
+}
